f616b264dc493bc8176f425a59b32af22e017485
Stanley Goldman <Stanley.Goldman@gmail.com>
2012-10-18 12:30:34 -0400

    Remove PublishProfileReader dependency

--- AzurePublishHelpers/Build Server Files/AzureDeploy.ps1
+++ AzurePublishHelpers/Build Server Files/AzureDeploy.ps1
@@ -9,12 +9,50 @@ Param (
   [Parameter(Mandatory=$False,Position=3)]
   [string]$buildTag
 )
+$ErrorActionPreference = "Stop"
 
-Import-Module PublishProfileReader
+Add-Type -AssemblyName System.Xml.Linq
+
+function Get-PublishProfile{
+	Param (
+	  [Parameter(Mandatory=$True,Position=0)]
+	  [string]$xmlFile
+	)
+
+	if (! [System.IO.File]::Exists($xmlFile))
+    {
+        return $null;
+    }
+
+	$doc = [xml] (Get-Content $xmlFile)
+	$propertyGroup = $doc.Project.PropertyGroup
+
+	$result = New-Object PSObject -Property @{
+		Credentials = $propertyGroup.AzureCredentials 
+        HostedServiceName = $propertyGroup.AzureHostedServiceName 
+        HostedServiceLabel = $propertyGroup.AzureHostedServiceLabel 
+        Slot = $propertyGroup.AzureSlot 
+        EnableIntelliTrace = ($propertyGroup.AzureEnableIntelliTrace -eq "True") 
+        EnableProfiling = ($propertyGroup.AzureEnableProfiling -eq "True") 
+        EnableWebDeploy = ($propertyGroup.AzureEnableWebDeploy -eq "True") 
+        StorageAccountName = $propertyGroup.AzureStorageAccountName 
+        StorageAccountLabel = $propertyGroup.AzureStorageAccountLabel 
+        DeploymentLabel = $propertyGroup.AzureDeploymentLabel 
+        SolutionConfiguration = $propertyGroup.AzureSolutionConfiguration 
+        ServiceConfiguration = $propertyGroup.AzureServiceConfiguration 
+        AppendTimestampToDeploymentLabel = ($propertyGroup.AzureAppendTimestampToDeploymentLabel -eq "True")
+        DeploymentReplacementMethod = $propertyGroup.AzureDeploymentReplacementMethod 
+        DeleteDeploymentOnFailure = ($propertyGroup.AzureDeleteDeploymentOnFailure -eq "True")
+        FallbackToDeleteAndRecreateIfUpgradeFails = ($propertyGroup.AzureFallbackToDeleteAndRecreateIfUpgradeFails -eq "True")
+        EnableRemoteDesktop = ($propertyGroup.AzureEnableRemoteDesktop -eq "True")
+	}
+
+	return $result
+}
 
 $publishProfileFullPath = Resolve-Path $publishProfilePath
-$publishProfile = [AzurePublishHelpers.PublishProfileReader.PublishProfileHelper]::GetPublishProfile($publishProfileFullPath)
-Write-Host Using PublishProfile: $publishProfile.ConnectionName
+$publishProfile = Get-PublishProfile $publishProfileFullPath
+Write-Host Using PublishProfile: $publishProfile.Credentials
 
 if ((Get-Module | ?{$_.Name -eq "Azure"}) -eq $null)
 {
@@ -56,16 +94,16 @@ function Wait-ForDeploymentInState {
     }
 }
 
-Set-AzureSubscription -SubscriptionName $publishProfile.ConnectionName
+Set-AzureSubscription -SubscriptionName $publishProfile.Credentials
 
-$existingDeployment = Get-AzureDeployment -ServiceName $publishProfile.HostedServiceName -Slot $publishProfile.DeploymentSlot
+$existingDeployment = Get-AzureDeployment -ServiceName $publishProfile.HostedServiceName -Slot $publishProfile.Slot
 if ($existingDeployment -ne $null)
 {
-	Set-AzureDeployment -Status -ServiceName $publishProfile.HostedServiceName -Slot $publishProfile.DeploymentSlot -NewStatus "Suspended"
+	Set-AzureDeployment -Status -ServiceName $publishProfile.HostedServiceName -Slot $publishProfile.Slot -NewStatus "Suspended"
 
-	Wait-ForDeploymentInState $publishProfile.HostedServiceName $publishProfile.DeploymentSlot "Suspended"
+	Wait-ForDeploymentInState $publishProfile.HostedServiceName $publishProfile.Slot "Suspended"
 
-	Remove-AzureDeployment -ServiceName $publishProfile.HostedServiceName -Slot $publishProfile.DeploymentSlot -Force
+	Remove-AzureDeployment -ServiceName $publishProfile.HostedServiceName -Slot $publishProfile.Slot -Force
 }
 
 $buildLabelArray = @($publishProfile.DeploymentLabel)
@@ -84,6 +122,6 @@ $buildLabel = [string]::Join(" - ", $buildLabelArray)
 $packagePath = $publishPath + $package
 $configurationPath = [string]::Format("{0}ServiceConfiguration.{1}.cscfg", $publishPath, $publishProfile.ServiceConfiguration)
 
-New-AzureDeployment -ServiceName $publishProfile.HostedServiceName -Package $packagePath -Configuration $configurationPath -Slot $publishProfile.DeploymentSlot -Label $buildLabel
+New-AzureDeployment -ServiceName $publishProfile.HostedServiceName -Package $packagePath -Configuration $configurationPath -Slot $publishProfile.Slot -Label $buildLabel
 
-Wait-ForDeploymentInState $publishProfile.HostedServiceName $publishProfile.DeploymentSlot "Running"
+Wait-ForDeploymentInState $publishProfile.HostedServiceName $publishProfile.Slot "Running"
